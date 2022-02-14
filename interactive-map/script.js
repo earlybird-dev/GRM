87cf6cabbf3774063f348bded70e58fba1a54b59
Ha Nguyen <31268683+trungha-ngx@users.noreply.github.com>
2022-02-15 08:24:27 +1100

    Refine CSS

--- interactive-map/script.js
+++ interactive-map/script.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict';
 require([
-  "esri/config",
-  "esri/Map",
-  "esri/views/MapView",
+  'esri/config',
+  'esri/Map',
+  'esri/views/MapView',
   // Locate your geolocation
-  "esri/widgets/Locate",
+  'esri/widgets/Locate',
   // Add the Basemap Toggle
-  "esri/widgets/BasemapToggle",
+  'esri/widgets/BasemapToggle',
   // Add a feature layer
-  "esri/layers/FeatureLayer",
+  'esri/layers/FeatureLayer',
   // Display a pie chart in popups
-  "esri/popup/content/support/ChartMediaInfoValue",
-  "esri/popup/content/PieChartMediaInfo",
-  "esri/popup/content/MediaContent",
+  'esri/popup/content/support/ChartMediaInfoValue',
+  'esri/popup/content/PieChartMediaInfo',
+  'esri/popup/content/MediaContent',
 ], function (
   esriConfig,
   Map,
@@ -25,7 +25,7 @@ require([
   MediaContent
 ) {
   esriConfig.apiKey =
-    "AAPK30b010ce8ade409994e766674699b35aGy8-P5kdFrGF0OancFBdXjLEV6HiCsd0jxVTU5Usgps-AXiWwRgN8cj-1jdzD4r8";
+    'AAPK30b010ce8ade409994e766674699b35aGy8-P5kdFrGF0OancFBdXjLEV6HiCsd0jxVTU5Usgps-AXiWwRgN8cj-1jdzD4r8';
 
   // SECTION: Create a map and map view
   // Ref: https://developers.arcgis.com/javascript/latest/display-a-map/
@@ -36,14 +36,14 @@ require([
   // Map: A map defines the layers that need to be displayed.
   const map = new Map({
     // basemap: "arcgis-navigation",
-    basemap: "streets-night-vector",
+    basemap: 'streets-night-vector',
   });
   const view = new MapView({
     map: map,
     center: CENTER_POINT,
     zoom: GLOBAL_ZOOM,
     // scale: GLOBAL_SCALE,
-    container: "map-view", // Set the HTML container property to display the contents of the map.
+    container: 'map-container', // Set the HTML container property to display the contents of the map.
     constraints: {
       snapToZoom: false,
       minScale: 250000000, // User cannot zoom out beyond a scale of 1:250,000,000
@@ -56,7 +56,7 @@ require([
     },
     background: {
       // autocasts new ColorBackground()
-      color: "#145374", // autocasts as new Color()
+      color: '#145374', // autocasts as new Color()
     },
     // resizing: true,
     popup: {
@@ -81,28 +81,28 @@ require([
       return view.goTo(options.target);
     },
   });
-  view.ui.add(locate, "top-left"); // Add to the view
+  view.ui.add(locate, 'top-left'); // Add to the view
 
   // SECTION: Toggle between basemaps
   // Ref: https://developers.arcgis.com/javascript/latest/change-the-basemap-layer/
   const basemapToggle = new BasemapToggle({
     view: view,
-    nextBasemap: "streets-vector",
+    nextBasemap: 'streets-vector',
 
-    label: "Toggle Between Basemaps",
+    label: 'Toggle Between Basemaps',
   });
-  view.ui.add(basemapToggle, "bottom-left"); // Add to the view
+  view.ui.add(basemapToggle, 'bottom-left'); // Add to the view
 
   // SECTION: Style markers and labels
   // Ref: https://developers.arcgis.com/javascript/latest/style-a-feature-layer/
   // Style project feature layer
   const projectMarkers = {
-    type: "simple",
+    type: 'simple',
     symbol: {
-      type: "picture-marker",
-      url: "https://i.ibb.co/JHfPVcL/leaf.png",
-      width: "20px",
-      height: "20px",
+      type: 'picture-marker',
+      url: 'https://i.ibb.co/JHfPVcL/leaf.png',
+      width: '20px',
+      height: '20px',
       //   xoffset: "10px",
     },
     visualVariables: [
@@ -119,43 +119,43 @@ require([
       //     maxSize: 12,
       //   },
       {
-        type: "color",
+        type: 'color',
         // Use {cluster_avg_TEMP} in the
         // featureReduction.popupTemplate to
         // display the average temperature of all
         // features within the cluster
-        field: "DirectBeneficiaries_HH",
+        field: 'DirectBeneficiaries_HH',
         stops: [
-          { value: 12500, color: "#fbfd00" },
-          { value: 25000, color: "#94c507" },
-          { value: 50000, color: "#649200" },
-          { value: 100000, color: "#517601" },
-          { value: 200000, color: "#1f4700" },
+          { value: 12500, color: '#fbfd00' },
+          { value: 25000, color: '#94c507' },
+          { value: 50000, color: '#649200' },
+          { value: 100000, color: '#517601' },
+          { value: 200000, color: '#1f4700' },
         ],
       },
     ],
   };
-  const haloColor = "#373837";
-  const color = "#f0f0f0";
-  const haloSize = "1.5px";
+  const haloColor = '#373837';
+  const color = '#f0f0f0';
+  const haloSize = '1.5px';
 
   const projectLabels = {
     symbol: {
-      type: "text",
+      type: 'text',
       haloColor,
       haloSize,
       color,
       font: {
-        size: "12px",
-        family: "Noto Sans",
-        style: "italic",
-        weight: "normal",
+        size: '12px',
+        family: 'Noto Sans',
+        style: 'italic',
+        weight: 'normal',
       },
     },
 
-    labelPlacement: "above-center",
+    labelPlacement: 'above-center',
     labelExpressionInfo: {
-      expression: "$feature.Proj_Code",
+      expression: '$feature.Proj_Code',
     },
   };
 
@@ -165,15 +165,15 @@ require([
   // Create a new PieChart to display within the PopupTemplate
   // Create the ChartMediaInfoValue
   let pieChartValue = new ChartMediaInfoValue({
-    fields: ["Men_Trained", "Women_Trained"],
+    fields: ['Men_Trained', 'Women_Trained'],
     normalizeField: null,
-    tooltipField: "<field name>",
+    tooltipField: '<field name>',
   });
 
   // Create the PieChartMediaInfo media type
   let pieChart = new PieChartMediaInfo({
-    title: "<b>People Trained</b>",
-    caption: "By Gender",
+    title: '<b>People Trained</b>',
+    caption: 'By Gender',
     value: pieChartValue,
   });
 
@@ -186,62 +186,62 @@ require([
   // Ref: https://developers.arcgis.com/javascript/latest/display-a-pop-up/
   // Create a project popup template
   const projectPopup = {
-    title: "{Proj_Status} Project by {Organisation}",
+    title: '{Proj_Status} Project by {Organisation}',
     content: [
       {
-        type: "text", // Text element
-        text: "{Description}",
+        type: 'text', // Text element
+        text: '{Description}',
       },
       {
-        type: "fields", // Table element
+        type: 'fields', // Table element
         fieldInfos: [
           {
-            fieldName: "Nation",
-            label: "Nation",
+            fieldName: 'Nation',
+            label: 'Nation',
           },
           {
-            fieldName: "Project_Value",
-            label: "Project Value",
+            fieldName: 'Project_Value',
+            label: 'Project Value',
             format: {
               digitSeparator: true,
               places: 0,
             },
           },
           {
-            fieldName: "Trees_mgmt",
-            label: "Tree Management",
+            fieldName: 'Trees_mgmt',
+            label: 'Tree Management',
             format: {
               digitSeparator: true,
               places: 0,
             },
           },
           {
-            fieldName: "Men_Trained",
-            label: "Men Trained",
+            fieldName: 'Men_Trained',
+            label: 'Men Trained',
             format: {
               digitSeparator: true,
               places: 0,
             },
           },
           {
-            fieldName: "Women_Trained",
-            label: "Women Trained",
+            fieldName: 'Women_Trained',
+            label: 'Women Trained',
             format: {
               digitSeparator: true,
               places: 0,
             },
           },
           {
-            fieldName: "DirectBeneficiaries_HH",
-            label: "Direct Beneficiaries",
+            fieldName: 'DirectBeneficiaries_HH',
+            label: 'Direct Beneficiaries',
             format: {
               digitSeparator: true,
               places: 0,
             },
           },
           {
-            fieldName: "InDirectBeneficiaries_HH",
-            label: "Indirect Beneficiaries",
+            fieldName: 'InDirectBeneficiaries_HH',
+            label: 'Indirect Beneficiaries',
             format: {
               digitSeparator: true,
               places: 0,
@@ -251,23 +251,23 @@ require([
       },
       piechartElement, // PieChart element
       {
-        type: "media", // Image/Chart element
+        type: 'media', // Image/Chart element
         mediaInfos: [
           {
-            title: "<b>Trees</b>",
-            type: "image",
-            caption: "trees",
+            title: '<b>Trees</b>',
+            type: 'image',
+            caption: 'trees',
             value: {
               sourceURL:
-                "https://www.sunset.com/wp-content/uploads/96006df453533f4c982212b8cc7882f5-800x0-c-default.jpg",
+                'https://www.sunset.com/wp-content/uploads/96006df453533f4c982212b8cc7882f5-800x0-c-default.jpg',
             },
           },
           {
-            title: "<b>Logo</b>",
-            type: "image",
-            caption: "logo",
+            title: '<b>Logo</b>',
+            type: 'image',
+            caption: 'logo',
             value: {
-              sourceURL: "../images/GRM.png",
+              sourceURL: '../images/GRM.png',
             },
           },
         ],
@@ -280,8 +280,8 @@ require([
   // Ref: https://developers.arcgis.com/javascript/latest/sample-code/featurereduction-cluster-filter-slider/
 
   const clusterConfig = {
-    type: "cluster",
-    clusterRadius: "80px",
+    type: 'cluster',
+    clusterRadius: '80px',
     // {cluster_count} is an aggregate field containing
     // the number of features comprised by the cluster
     // popupTemplate: {
@@ -297,58 +297,58 @@ require([
     //     },
     //   ],
     // },
-    clusterMinSize: "30px",
-    clusterMaxSize: "60px",
+    clusterMinSize: '30px',
+    clusterMaxSize: '60px',
     labelingInfo: [
       {
-        deconflictionStrategy: "none",
+        deconflictionStrategy: 'none',
         labelExpressionInfo: {
           expression: "Text($feature.cluster_count, '#,###')",
         },
         symbol: {
-          type: "text",
+          type: 'text',
           haloColor,
           haloSize,
           color,
           font: {
-            weight: "bold",
-            family: "Noto Sans",
-            size: "20px",
+            weight: 'bold',
+            family: 'Noto Sans',
+            size: '20px',
           },
         },
-        labelPlacement: "center-center",
+        labelPlacement: 'center-center',
       },
     ],
   };
   // SECTION: Create a project point layer
   // Create the projects feature layer and set the renderer, labels, popup template.
   const projectLayer = new FeatureLayer({
-    url: "https://services9.arcgis.com/h8H4fa0wsbwmIt3l/arcgis/rest/services/GRM_Projects/FeatureServer/0",
+    url: 'https://services9.arcgis.com/h8H4fa0wsbwmIt3l/arcgis/rest/services/GRM_Projects/FeatureServer/0',
     renderer: projectMarkers,
     labelingInfo: [projectLabels],
     popupTemplate: projectPopup,
     featureReduction: clusterConfig,
     outFields: [
-      "Nation",
-      "Organisation",
-      "Description",
-      "x",
-      "y",
-      "Proj_Status",
-      "Donor_Principal",
-      "Implementing_Partners",
-      "Start_Year",
-      "End_Year",
-      "Land_Area",
-      "Trees_mgmt",
-      "Tree_Density",
-      "Practice",
+      'Nation',
+      'Organisation',
+      'Description',
+      'x',
+      'y',
+      'Proj_Status',
+      'Donor_Principal',
+      'Implementing_Partners',
+      'Start_Year',
+      'End_Year',
+      'Land_Area',
+      'Trees_mgmt',
+      'Tree_Density',
+      'Practice',
     ],
   });
 
   map.add(projectLayer); // Add project layer to map
 
-  // SECTION: Access featyres with pointer events
+  // SECTION: Access features with pointer events
   // Ref: https://developers.arcgis.com/javascript/latest/sample-code/view-hittest/
   // SECTION: Highlight points with pointer events
   // Ref: https://developers.arcgis.com/javascript/latest/sample-code/view-hittest/
@@ -370,8 +370,8 @@ require([
       return view.whenLayerView(layer);
     })
     .then((layerView) => {
-      view.on("pointer-move", eventHandler);
-      view.on("pointer-down", eventHandler);
+      view.on('pointer-move', eventHandler);
+      view.on('pointer-down', eventHandler);
 
       function eventHandler(event) {
         // only include graphics from hurricanesLayer in the hitTest
@@ -393,8 +393,8 @@ require([
           const graphic = response.results[0].graphic;
           const attributes = graphic.attributes;
           console.log(attributes);
-          if (Object.keys(attributes).includes("clusterId")) {
-            console.log("Cluster");
+          if (Object.keys(attributes).includes('clusterId')) {
+            console.log('Cluster');
             return;
           }
           const projectStatus = attributes.Proj_Status;
@@ -411,11 +411,12 @@ require([
             return;
           }
 
-          document.getElementById("project-info").style.visibility = "visible";
-          document.getElementById("project-code").innerHTML =
-            "Project Code: " + projectCode;
-          document.getElementById("project-status").innerHTML =
-            "Status: " + projectStatus;
+          document.querySelector('.project-info').style.visibility = 'visible';
+
+          document.getElementById('project-code').innerHTML =
+            'Project Code: ' + projectCode;
+          document.getElementById('project-status').innerHTML =
+            'Status: ' + projectStatus;
 
           // highlight all features belonging to the same hurricane as the feature
           // returned from the hitTest
@@ -427,6 +428,9 @@ require([
             }
             highlight = layerView.highlight(ids);
             currentProject = projectCode;
+
+            document.getElementById('sites').innerHTML =
+              'Number of Sites: ' + ids.length + ' (' + [...ids] + ')';
           });
         } else {
           // remove the highlight if no features are
@@ -435,7 +439,7 @@ require([
             highlight.remove();
             highlight = null;
           }
-          document.getElementById("project-info").style.visibility = "hidden";
+          document.querySelector('.project-info').style.visibility = 'hidden';
         }
       }
     });
@@ -450,25 +454,25 @@ require([
     duration: 1000, // Duration of animation will be 5 seconds
   };
 
-  const projectDataElement = document.querySelector(".project-data");
+  const projectDataElement = document.querySelector('.project-data');
 
   // Get the data from the click event
-  view.on("click", function (event) {
+  view.on('click', function (event) {
     // Get the clicked coordinates
     const mapPoint = event.mapPoint;
-    console.log("view.zoom", view.zoom);
-    console.log("event", event);
-    console.log("mapPoint", mapPoint);
+    console.log('view.zoom', view.zoom);
+    console.log('event', event);
+    console.log('mapPoint', mapPoint);
     console.log(`x: ${mapPoint.longitude}, y: ${mapPoint.latitude}`);
 
     view.hitTest(event).then(function (response) {
-      console.log("response.results", response.results);
+      console.log('response.results', response.results);
       // Check if clicked on graphic
       if (response.results.length >= 2) {
         const projectData = response.results[0].graphic.attributes;
-        console.log("projectData", projectData);
+        console.log('projectData', projectData);
         // Check if the graphic element is a cluster
-        if (Object.keys(projectData).includes("clusterId")) {
+        if (Object.keys(projectData).includes('clusterId')) {
           view.goTo(
             {
               center: [mapPoint.longitude, mapPoint.latitude],
@@ -488,12 +492,12 @@ require([
         );
 
         // Display project data in the project tab
-        projectDataElement.innerHTML = "";
-        const header = document.createElement("h2");
-        header.innerHTML = "About Project";
+        projectDataElement.innerHTML = '';
+        const header = document.createElement('h2');
+        header.innerHTML = 'About Project';
         projectDataElement.appendChild(header);
 
-        const ignoredFields = ["ObjectId", "Proj_Code", "x", "y"];
+        const ignoredFields = ['ObjectId', 'Proj_Code', 'x', 'y'];
         Object.entries(projectData).forEach(function (item) {
           const [key, value] = item;
           if (ignoredFields.includes(key)) {
@@ -502,8 +506,8 @@ require([
           if (!value) {
             return;
           }
-          const subHeader = document.createElement("h3");
-          const content = document.createElement("p");
+          const subHeader = document.createElement('h3');
+          const content = document.createElement('p');
           subHeader.innerHTML = key;
           content.innerHTML = value;
           projectDataElement.appendChild(subHeader);
@@ -514,23 +518,23 @@ require([
   });
 
   // SECTION: Hide/Show Project Button
-  const hideProjectButton = document.querySelector(".hide-tab-btn");
+  const hideProjectButton = document.querySelector('.hide-tab-btn');
   let hide = false;
-  hideProjectButton.addEventListener("click", function () {
+  hideProjectButton.addEventListener('click', function () {
     if (!hide) {
-      document.querySelector(".body-view").classList.add("hidden");
-      hideProjectButton.innerHTML = "Show Project Tab";
+      document.querySelector('.sidebar-container').classList.add('hidden');
+      hideProjectButton.innerHTML = 'Show Project Tab';
       hide = true;
     } else {
-      document.querySelector(".body-view").classList.remove("hidden");
-      hideProjectButton.innerHTML = "Hide Project Tab";
+      document.querySelector('.sidebar-container').classList.remove('hidden');
+      hideProjectButton.innerHTML = 'Hide Project Tab';
       hide = false;
     }
   });
 
   // SECTION: Reset Map View Button
-  const resetMapViewButton = document.querySelector(".reset-view-btn");
-  resetMapViewButton.addEventListener("click", function () {
+  const resetMapViewButton = document.querySelector('.reset-view-btn');
+  resetMapViewButton.addEventListener('click', function () {
     projectDataElement.innerHTML = `<h2>GRM Interactive Dashboard</h2>
     <p>
       A key focus of the GRM is to provide clear visibility to existing
